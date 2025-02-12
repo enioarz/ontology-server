891ff215ce9bd8edd0c83138cab160d99d546c46
Eugenio Salvador Arellano Ruiz <euarellano2192@gmail.com>
2025-02-12 21:20:57 +0100

    Cleaned main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use curie::PrefixMapping;
 use horned_owl::io::owx::reader::read_with_build;
 use horned_owl::model::Build;
 use horned_owl::visitor::immutable::Walk;
@@ -9,17 +8,12 @@ fn main() {
     let r = read_with_build(ont_s.as_bytes(), &Build::new_string());
     assert!(r.is_ok(), "Expected ontology, got failure:{:?}", r.err());
     let (o, pm) = r.ok().unwrap();
-    // let mut pm = PrefixMapping::default();
-    // pm.add_prefix("skos", "http://www.w3.org/2004/02/skos/core#")
-    //     .unwrap();
-    // pm.add_prefix("dc11", "http://purl.org/dc/elements/1.1/")
-    //     .unwrap();
     let oc = OntologyContent::new_with_prefix_mapping(pm);
     let mut walk = Walk::new(oc);
     walk.set_ontology(&o);
     let visit = walk.into_visit();
     let md = &visit.metadata.render_html().unwrap();
-    let mut hm: Vec<String> = visit
+    let mut _hm: Vec<String> = visit
         .into_hashmap()
         .values()
         .map(|x| x.render_html().unwrap())
